b85784395542d0b6eb0e4147427713f8d35e8912
Luke M Griffith <lukemgriffith@gmail.com>
2015-10-31 21:06:15 +0000

    major refactor
    breaking changes to module. now much more of a wrapper for
    SqlConnection.

--- Power-SQL.psm1
+++ Power-SQL.psm1
@@ -1,16 +1,122 @@
-﻿<#
+﻿# Importing the module creates a new private connection object.
+$conn = New-Object System.Data.SqlClient.SqlConnection;
+$closed = $true
 
-PowerSQL is used to help query SQL Servers, by generating connection strings, obtaining tables and columns then allowing you to query.
-Build on PowerShell v5
+Function Open-SQLConnection {
+    [cmdletbinding()]
+    param(
+        [parameter(ParameterSetName="ConnectionString",Mandatory=$true,HelpMessage="Specify full connection string.")]
+        [String]$ConnectionString,
+        [parameter(ParameterSetName="ConnectionBuilder",Mandatory=$true,HelpMessage="Server name, specify instace <server>\<instance>")]
+        [String]$Server,
+        [parameter(ParameterSetName="ConnectionBuilder",HelpMessage="Specify database name.")]
+        [String]$Database,
+        [parameter(ParameterSetName="ConnectionBuilder",HelpMessage="provide a PS Credential object, if left empty expects a trusted connection.")]
+        [PSCredential]$Credential
+    )
 
-#>
+    if ($script:closed -eq $true) {
 
-# Imports PS1 files from script root
-if ($psversiontable.psversion.major -eq 5)
-{
-Get-ChildItem -Path $PSScriptRoot\*.ps1 | Foreach-Object{ . $_.FullName }	
+        if ($PSCmdlet.ParameterSetName -eq "ConnectionString") {
+            Write-Verbose -Message "Opening connection with specified connection string"
+            $script:conn.ConnectionString = $ConnectionString;
+            $script:conn.open();
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq "ConnectionBuilder") {
+
+            $db = {if($Database){"with database $($Database)"}}
+
+            if ($Credential) {
+                Write-Verbose -Message "Opening connection with credential object to $($Server) $(icm $db )"
+                $ConnectionString = "Server={0};Database={1};User Id={2};Password={3};" -f $Server, $Database, 
+                    $Credential.GetNetworkCredential().UserName, $Credential.GetNetworkCredential().Password
+            } else {
+                Write-Verbose -Message "Opening trusted connection to $($Server) $(icm $db)"
+                $ConnectionString = "Server={0};Database={1};User Id={2};Trusted_Connection=yes;" -f $Server, $Database
+                    
+            }
+
+            $script:conn.ConnectionString = $ConnectionString;
+            $script:conn.open();
+        }
+
+    } 
+    else {
+        Write-Error -Message "Previous connection open" -Exception PowerSQL.ExistingConnection
+        Break
+    }
+
+    $script:closed = $false
+    
 }
-elseif ($psversiontable.psversion.major -eq 4){
-Get-ChildItem $PSScriptRoot\*ps1 | Where-Object name -notlike "Class*" | Foreach-Object{ . $_.FullName }	
+
+Function Set-SQLDatabase {
+    [cmdletbinding()]
+    param(
+        $database
+    )
+
+    try {
+        Write-Verbose "Changing database from $($script:conn.Database) to $database"
+        $script:conn.ChangeDatabase($database)
+    } 
+    catch {
+        Write-Error "$($_.exception)" -Exception PowerSQL.ChangeDatabaseError
+    }
+
+}
+
+Function Get-SQLConnection {
+    [cmdletbinding()]
+    param(
+
+    )
+
+    $script:conn
+    
+}
+
+Function Invoke-Query {
+    [cmdletbinding()]
+    param(
+        [string]$query
+    )
+
+    Write-Verbose "executing query against database $($script:conn.database)"
+    try {
+        $command = $script:conn.CreateCommand()
+        $command.CommandText = $query
+    }
+    catch {
+        Write-Error "Unable to create DBcommand"
+        break
+    }
+
+    Write-Verbose "Creating new datatable"
+    $table = new-object “System.Data.DataTable”
+
+    Write-Verbose "Executing reader"
+    $data = $command.ExecuteReader()
+
+    Write-Verbose "Loading reader result into datatable"
+    $table.load($data)
+
+    $table
+
+
+}
+
+Function Close-SQLConnection {
+    [cmdletbinding()]
+    param()
+    try {
+        Write-Verbose "Closing connection to $($script:conn.DataSource)"
+        $script:conn.Close()
+    }
+    catch {
+        Write-Error "Could not close SQL connection" -Exception PowerSQL.CouldNotClose
+        break
+    }
+    $script:closed = $true
 
 }
