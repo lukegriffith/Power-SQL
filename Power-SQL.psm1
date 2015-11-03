d90a7fc215002209d04980e8f44396cc025e4d5c
Luke M Griffith <lukemgriffith@gmail.com>
2015-11-03 22:51:58 +0000

    Breaking Changes V1.1
    Boom - well not a full version but half of it - turned $script:conn into
    an array, and added the basic functionality to allow for
    Get-SQLConnection and New-SQLConnection to work, along with
    Set-Database, now you can have multiple SQL connections in the module.
    
    Further work to be done - currently this is not in a working state as
    Close and Invoke are still on the old way - this is more of a half
    version. More to follow.

--- Power-SQL.psm1
+++ Power-SQL.psm1
@@ -1,64 +1,84 @@
 ﻿# Importing the module creates a new private connection object.
-$conn = New-Object System.Data.SqlClient.SqlConnection;
-$closed = $true
+# TO DO 
+# 1. Update Close and Invoke as they currently do not work in the net setup.
+# 2. Allow for pipeline input for ALL cmdlets, besides New-SQLConnection... Maybe Get can take pipeline input for name? 
+# 3. Create pester tests for module to ensure functionality works.
+# 4. Work on naming conventions.
+
+$script:conn = @()
+
+
+Function New-SQLConnection {
+
+    param(
+    [parameter(Mandatory=$true,HelpMessage="Name of connection.")]
+    [string]$Name,
+    [parameter(ParameterSetName="ConnectionString",Mandatory=$true,HelpMessage="Specify full connection string.")]
+    [String]$ConnectionString,
+    [parameter(ParameterSetName="ConnectionBuilder",Mandatory=$true,HelpMessage="Server name, specify instace <server>\<instance>")]
+    [String]$Server,
+    [parameter(ParameterSetName="ConnectionBuilder",HelpMessage="Specify database name.")]
+    [String]$Database,
+    [parameter(ParameterSetName="ConnectionBuilder",HelpMessage="provide a PS Credential object, if left empty expects a trusted connection.")]
+    [PSCredential]$Credential
+    )
+
+    $conn = New-Object System.Data.SqlClient.SqlConnection;
+    $conn | Add-Member -MemberType NoteProperty -Name Name -Value $Name
+
+    if ($PSCmdlet.ParameterSetName -eq "ConnectionString") {
+        Write-Verbose -Message "Opening connection with specified connection string"
+        $conn.ConnectionString = $ConnectionString;
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "ConnectionBuilder") {
+
+        $db = {if($Database){"with database $($Database)"}}
+
+        if ($Credential) {
+            Write-Verbose -Message "Opening connection with credential object to $($Server) $(icm $db )"
+            $ConnectionString = "Server={0};Database={1};User Id={2};Password={3};" -f $Server, $Database, 
+                $Credential.GetNetworkCredential().UserName, $Credential.GetNetworkCredential().Password
+        } else {
+            Write-Verbose -Message "Opening trusted connection to $($Server) $(icm $db)"
+            $ConnectionString = "Server={0};Database={1};Trusted_Connection=yes;" -f $Server, $Database
+                
+        }
+
+        $conn.ConnectionString = $ConnectionString;
+    }
+
+    $Script:conn += $conn
+
+}
 
 Function Open-SQLConnection {
     [cmdletbinding()]
     param(
-        [parameter(ParameterSetName="ConnectionString",Mandatory=$true,HelpMessage="Specify full connection string.")]
-        [String]$ConnectionString,
-        [parameter(ParameterSetName="ConnectionBuilder",Mandatory=$true,HelpMessage="Server name, specify instace <server>\<instance>")]
-        [String]$Server,
-        [parameter(ParameterSetName="ConnectionBuilder",HelpMessage="Specify database name.")]
-        [String]$Database,
-        [parameter(ParameterSetName="ConnectionBuilder",HelpMessage="provide a PS Credential object, if left empty expects a trusted connection.")]
-        [PSCredential]$Credential
+    [parameter(Mandatory=$true,HelpMessage="Name of connection.")]
+    [string]$Name
     )
 
-    if ($script:closed -eq $true) {
-
-        if ($PSCmdlet.ParameterSetName -eq "ConnectionString") {
-            Write-Verbose -Message "Opening connection with specified connection string"
-            $script:conn.ConnectionString = $ConnectionString;
-            $script:conn.open();
-        }
-        elseif ($PSCmdlet.ParameterSetName -eq "ConnectionBuilder") {
-
-            $db = {if($Database){"with database $($Database)"}}
-
-            if ($Credential) {
-                Write-Verbose -Message "Opening connection with credential object to $($Server) $(icm $db )"
-                $ConnectionString = "Server={0};Database={1};User Id={2};Password={3};" -f $Server, $Database, 
-                    $Credential.GetNetworkCredential().UserName, $Credential.GetNetworkCredential().Password
-            } else {
-                Write-Verbose -Message "Opening trusted connection to $($Server) $(icm $db)"
-                $ConnectionString = "Server={0};Database={1};User Id={2};Trusted_Connection=yes;" -f $Server, $Database
-                    
-            }
-
-            $script:conn.ConnectionString = $ConnectionString;
-            $script:conn.open();
-        }
-
-    } 
-    else {
-        Write-Error -Message "Previous connection open" -Exception PowerSQL.ExistingConnection
-        Break
-    }
-
-    $script:closed = $false
+    $conn = $script:conn | Where-Object {$_.name -like $Name} 
+    $conn.open()    
     
 }
+
 
 Function Set-SQLDatabase {
     [cmdletbinding()]
     param(
-        $database
+        [Parameter(Mandatory=$true)]
+        [string]$name,
+        [Parameter(Mandatory=$true)]
+        [string]$database
     )
 
+
+    $conn = $script:conn | Where-Object {$_.name -eq $name}
+
     try {
-        Write-Verbose "Changing database from $($script:conn.Database) to $database"
-        $script:conn.ChangeDatabase($database)
+        Write-Verbose "Changing database from $($conn.Database) to $database"
+        $conn.ChangeDatabase($database)
     } 
     catch {
         Write-Error "$($_.exception)" -Exception PowerSQL.ChangeDatabaseError
@@ -69,18 +89,21 @@ Function Set-SQLDatabase {
 Function Get-SQLConnection {
     [cmdletbinding()]
     param(
-
+    [string]$name
     )
 
+    if ($name) {
+    $script:conn | Where-Object {$_.name -eq $name} 
+    }
+    else {
     $script:conn
-    
+    }
+
 }
 
-Function Invoke-Query {
+Function Invoke-SQLQuery {
     [cmdletbinding()]
-    param(
-        [string]$query
-    )
+    param([string]$query)
 
     Write-Verbose "executing query against database $($script:conn.database)"
     try {
@@ -120,3 +143,4 @@ Function Close-SQLConnection {
     $script:closed = $true
 
 }
+    
